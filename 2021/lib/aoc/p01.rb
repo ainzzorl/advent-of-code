95a1dcd4739da966c6499d85b871b773f65b6e38
ainzzorl <ainzzorl@gmail.com>
2021-12-02 22:54:56 +0300

    Add solution to day 1 part 2

--- 2021/lib/aoc/p01.rb
+++ 2021/lib/aoc/p01.rb
@@ -1,7 +1,13 @@
 module AOC
-  def self.p01(vals)
+  def self.p01_1(vals)
     (1...vals.size)
       .select { |i| vals[i] > vals[i - 1] }
       .size
+  end
+
+  def self.p01_2(vals)
+    triplets = (1...vals.size - 1)
+               .map { |i| vals[i - 1] + vals[i] + vals[i + 1] }
+    p01_1(triplets)
   end
 end
--- 2021/spec/p01_spec.rb
+++ 2021/spec/p01_spec.rb
@@ -1,10 +1,23 @@
-RSpec.describe '#p01' do
-  it 'solves the example' do
-    expect(AOC.p01([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])).to eq 7
+RSpec.describe 'p01' do
+  describe 'part 1' do
+    it 'solves the example' do
+      expect(AOC.p01_1([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])).to eq 7
+    end
+
+    it 'solves the real input' do
+      input = AOC.read_ints_from_file('inputs/day01')
+      expect(AOC.p01_1(input)).to eq 1559
+    end
   end
 
-  it 'solves the real input' do
-    input = AOC.read_ints_from_file('inputs/day01')
-    expect(AOC.p01(input)).to eq 1559
+  describe 'part 2' do
+    it 'solves the example' do
+      expect(AOC.p01_2([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])).to eq 5
+    end
+
+    it 'solves the real input' do
+      input = AOC.read_ints_from_file('inputs/day01')
+      expect(AOC.p01_2(input)).to eq 1600
+    end
   end
 end
